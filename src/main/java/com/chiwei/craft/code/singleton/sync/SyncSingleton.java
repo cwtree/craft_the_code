48507c0461ff7ced15cc27aa123df53da52b99df
chiwei <chiwei@cmhi.chinamobile.com>
2019-03-21 22:24:08 +0800

    单例模式
    单例模式

---
+++ src/main/java/com/chiwei/craft/code/singleton/sync/SyncSingleton.java
@@ -0,0 +1,22 @@
+package com.chiwei.craft.code.singleton.sync;
+
+public class SyncSingleton {
+
+	private static SyncSingleton instance;
+
+	private SyncSingleton() {
+
+	}
+
+	/**
+	 * 存在性能问题，因为调用getInstance方法的地方都会同步阻塞一下 
+	 * @return
+	 */
+	public static synchronized SyncSingleton getInstance() {
+		if (instance == null) {
+			instance = new SyncSingleton();
+		}
+		return instance;
+	}
+
+}
