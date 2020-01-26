a064f89396459598ad9c8064e0f7255e7a60356d
chiwei <chiwei@cmhi.chinamobile.com>
2020-01-26 19:45:37 +0800

    strategy meeting thread
    strategy meeting thread

---
+++ src/main/java/com/chiwei/craft/code/strategy/WalkTravelStrategy.java
@@ -0,0 +1,11 @@
+package com.chiwei.craft.code.strategy;
+
+public class WalkTravelStrategy implements TravelStrategy {
+
+	@Override
+	public String travel() {
+		// TODO Auto-generated method stub
+		return "走路";
+	}
+
+}
