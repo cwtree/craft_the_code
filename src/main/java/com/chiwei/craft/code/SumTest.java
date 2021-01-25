6d33015b457df9506d399b83ecd60a025a54d3fd
cwtree <cwtree@mail.ustc.edu.cn>
2021-01-25 20:16:51 +0800

    update
    update

---
+++ src/main/java/com/chiwei/craft/code/SumTest.java
@@ -0,0 +1,10 @@
+package com.chiwei.craft.code;
+
+import java.util.stream.Stream;
+
+public class SumTest {
+
+    public static void main(String[] args) {
+    	System.out.println(Long.MAX_VALUE);
+    }
+}
---
+++ src/main/java/com/chiwei/craft/code/time/TimeTest.java
@@ -0,0 +1,21 @@
+package com.chiwei.craft.code.time;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class TimeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate now = LocalDate.now();
+		System.out.println(now);
+		LocalDateTime now2 = LocalDateTime.now();
+		System.out.println(now2);
+		LocalTime now3 = LocalTime.now();
+		System.out.println(now3);
+		int h=1;
+		System.out.println(h>>>16);
+	}
+
+}
